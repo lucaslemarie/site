86e5c0183818938c657df218cf07366aa4f5bc2c
lucaslemarie <73873995+lucaslemarie@users.noreply.github.com>
2020-11-30 17:17:11 +0100

    30/11/2020.22

--- src/datas.php
+++ src/datas.php
@@ -1,3 +1,3 @@
 <?php
-$menu=['Lemarié Lucas','<a href="#propos">A propos</a>','Compétences','Expérience','Formation','Contact'];
+$menu=['Lemarié Lucas','<a href="#propos">A propos</a>','<a href="#comp">Compétences','<a href="#exp">Expérience</a>','<a href="#formation">Formation</a>','<a href="#cont">Contact</a>'];
 ?>
