146f5df0c165761f7742dad85ce7fae528a5746d
lucaslemarie <73873995+lucaslemarie@users.noreply.github.com>
2020-11-30 17:24:20 +0100

    30/11/2020.23

--- src/datas.php
+++ src/datas.php
@@ -1,3 +1,4 @@
 <?php
 $menu=['Lemarié Lucas','<a href="#propos">A propos</a>','<a href="#comp">Compétences','<a href="#exp">Expérience</a>','<a href="#formation">Formation</a>','<a href="#cont">Contact</a>'];
+
 ?>
--- src/propos.php
+++ src/propos.php
@@ -1,8 +1,8 @@
-<div class="center">
+<div id="center">
   <h2 id="propos">A propos</h2>
   <img src="assets/img/moi.jpg" alt="" id="moi"/><br>
   <div id="prop">
   <p id='apropos'>Je m'appelle Lucas Lemarié, j'ai 18 ans et je suis en formation à Supavenir Sainte Ursule Caen pour obtenir un BTS Service Informatique aux Organisations. Je fais du piano depuis 5 ans et trouve que savoir lire une partition est un peu une façon de décoder un langage comme en informatique.Je me suis équipé d’un nouvel ordinateur et d’un kit arduino (comprenant notamment une carte arduino, des LED, un module LCD, un moteur, un module RFID, des résistances et des boutons.) pour approfondir mes connaissances en langage C++.</p>
 </div>
-</div>
+
   <a href="#comp"><i class="fas fa-angle-down fa-5x"></i></a>
