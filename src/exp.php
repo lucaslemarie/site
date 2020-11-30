442acddc98920a6cb8132c758c64fd68c13ef9f3
lucaslemarie <73873995+lucaslemarie@users.noreply.github.com>
2020-11-30 16:13:02 +0100

    30/11/2020.6

--- src/exp.php
+++ src/exp.php
@@ -4,7 +4,7 @@
 <li><h3>STAGE CHEZ CHAROZE DU 12 AU 16 DECEMBRE 2016</h3></li>
 <li><img src="assets/img/charoze.jpg" alt=""></li>
 <li><a href="https://www.google.fr/maps/place/Charoz%C3%A9+Apiculture/@49.1622252,-0.3121212,16z/data=!4m8!1m2!2m1!1sCharoz%C3%A9,+14+Rue+Charles+Coulomb+14120+MONDEVILLE+France!3m4!1s0x480a429656082c43:0x5e6abba7d4f6d600!8m2!3d49.1622252!4d-0.3084403"><i class="fas fa-map-marker-alt"></i>Lieu</a></li>
-<li>Je me suis occupé de l'inventaire du magasin</li><br>
+<li>Je me suis occupé de l'inventaire du magasin</li>
 <li><h3>Mini projet en Sti2d</h3></li>
 <li><img src="assets/img/lemonnier.png" alt="" class="lemonnier"></li>
 <li><p>Notre projet était de contrôler une affiche publicitaire à distance.</p></li>
