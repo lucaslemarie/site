049f3226532aad97a5c3698e78e8a3874c15c794
lucaslemarie <73873995+lucaslemarie@users.noreply.github.com>
2020-11-30 18:05:15 +0100

    30/11/2020.27

--- src/fonctions.php
+++ src/fonctions.php
@@ -7,5 +7,28 @@ function getMenu(array $items){
     $res.='</ul>';
     echo $res;
 }
-
+function getMenu1(array $items){
+    $res='<ul class="social">';
+    foreach($items as $item){
+        $res.="<li>$item</li>";
+    }
+    $res.='</ul>';
+    echo $res;
+}
+function getMenu2(array $items){
+    $res='<ul id="exper">';
+    foreach($items as $item){
+        $res.="<li>$item</li>";
+    }
+    $res.='</ul>';
+    echo $res;
+}
+function getMenu3(array $items){
+    $res='<ul id="form">';
+    foreach($items as $item){
+        $res.="<li>$item</li>";
+    }
+    $res.='</ul>';
+    echo $res;
+}
 ?>
